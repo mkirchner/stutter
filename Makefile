90e5bf1af53a45d69708fe45d73e15944ce01ebc
Marc Kirchner <mail@marc-kirchner.de>
2019-04-20 18:38:15 -0700

    initial import

---
+++ Makefile
@@ -0,0 +1,36 @@
+#
+# stutter Makefile
+#
+
+CC=clang
+CFLAGS=-g -Wall -Wextra -pedantic
+LDFLAGS=-g
+LDLIBS=-ledit
+RM=rm
+BINARY=stutter
+BUILD_DIR=./build
+SRCS=prompt.c
+OBJS=$(SRCS:%.c=$(BUILD_DIR)/%.o)
+DEPS=$(OBJS:%.o=%.d)
+
+all: $(BUILD_DIR)/$(BINARY)
+
+$(BUILD_DIR)/$(BINARY): $(OBJS)
+	mkdir -p $(@D)
+	$(CC) $(LDFLAGS) $(LDLIBS) $^ -o $@
+
+-include $(DEPS)
+
+$(BUILD_DIR)/%.o: %.c
+	mkdir -p $(@D)
+	$(CC) $(CFLAGS) -MMD -c $< -o $@
+
+.PHONY: clean
+clean:
+	$(RM) -f $(OBJS) $(DEPS)
+
+distclean: clean
+	$(RM) -f $(BUILD_DIR)/$(BINARY)
+
+# vim:ft=make
+#
---
+++ prompt.c
@@ -0,0 +1,33 @@
+/*
+ * prompt.c
+ * Copyright (C) 2019 mk <mk@marvin>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+
+#define __STUTTER_VERSION__ "0.0.1-alpha"
+
+int main(int argc, char* argv[]) {
+    printf("Stutter version %s\n\n", __STUTTER_VERSION__);
+
+
+    while(1) {
+        char* input = readline("stutter> ");
+	if (input == NULL) {
+	   return 0;
+	}
+        add_history(input);
+        printf("You said: %s\n", input);
+        free(input);
+    }
+
+    return 0;
+}
+
+
+
