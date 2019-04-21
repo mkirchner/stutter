858fad01e084ed72bc69b2bdda7bebba1df25f03
Marc Kirchner <marc_kirchner@apple.com>
2019-04-20 19:35:33 -0700

    Applying KR style

--- prompt.c
+++ prompt.c
@@ -1,6 +1,6 @@
 /*
  * prompt.c
- * Copyright (C) 2019 mk <mk@marvin>
+ * Copyright (C) 2019 Marc Kirchner
  *
  * Distributed under terms of the MIT license.
  */
@@ -12,22 +12,19 @@
 
 #define __STUTTER_VERSION__ "0.0.1-alpha"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     printf("Stutter version %s\n\n", __STUTTER_VERSION__);
-
 
     while(1) {
         char* input = readline("stutter> ");
-	if (input == NULL) {
-	   return 0;
-	}
+        if (input == NULL) {
+            return 0;
+        }
         add_history(input);
         printf("You said: %s\n", input);
         free(input);
     }
-
     return 0;
 }
-
-
 
